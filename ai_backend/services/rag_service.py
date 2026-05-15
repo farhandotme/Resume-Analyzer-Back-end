c0a9090f021540b94e0a1f4681687c333648bf12
farhandotme <princeofficial355@gmail.com>
2026-05-15 22:43:09 +0530

    done with rag_pdf_upload , loading the pdf, split it , chunk it and store it into the qdrant db

--- ai_backend/services/rag_service.py
+++ ai_backend/services/rag_service.py
@@ -4,6 +4,10 @@ import requests
 from pydantic import BaseModel
 import os
 from fastapi import APIRouter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+import asyncio
 
 router = APIRouter()
 
@@ -14,13 +18,34 @@ load_dotenv()
 
 class PdfData(BaseModel):
     pdf_url: str
+    user_id: str
 
 
-async def rag_service(req: PdfData):
+@router.post("/rag-pdf")
+async def rag_storing_pdf(req: PdfData):
     pdfUrl = req.pdf_url
+    user_id = req.user_id
     print("url :", pdfUrl)
+    # loading the pdf---->
+
     loader = PyPDFLoader(pdfUrl)
     data = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-    texts = text_splitter.split_text(data)
-    
+
+    # text splitter--->
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    texts = text_splitter.split_documents(data)
+
+    # embedding Model -------
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    # client = QdrantClient(host="localhost", port=6333)
+    vector_store = QdrantVectorStore.from_documents(
+        documents=texts,
+        embedding=embeddings,
+        url="http://localhost:6333",
+        collection_name=f"resume_{user_id}",
+    )
+    return {"message": "Resume stored successfully", "collection": f"resume_{user_id}"}
