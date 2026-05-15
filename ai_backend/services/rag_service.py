72f3f94e0c4852d4cb21b2f8cf3b6bcafcff9697
farhandotme <princeofficial355@gmail.com>
2026-05-16 00:03:52 +0530

    created that if the user uploads a new pdf then the function will delete the old data and use the new data

--- ai_backend/services/rag_service.py
+++ ai_backend/services/rag_service.py
@@ -2,12 +2,10 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 import requests
 from pydantic import BaseModel
-import os
 from fastapi import APIRouter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
-import asyncio
 
 router = APIRouter()
 
@@ -21,6 +19,9 @@ class PdfData(BaseModel):
     user_id: str
 
 
+# this is the pdf storing function that used to first load the pdf , then split it , generate the embedding and then store ot into the qdrant db
+
+
 @router.post("/rag-pdf")
 async def rag_storing_pdf(req: PdfData):
     pdfUrl = req.pdf_url
@@ -30,6 +31,8 @@ async def rag_storing_pdf(req: PdfData):
 
     loader = PyPDFLoader(pdfUrl)
     data = loader.load()
+
+    collection_name = f"resume_{user_id}"
 
     # text splitter--->
 
@@ -41,11 +44,18 @@ async def rag_storing_pdf(req: PdfData):
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    # client = QdrantClient(host="localhost", port=6333)
+    client = QdrantClient(url="http://localhost:6333")
+    collections = client.get_collections().collections
+
+    existing_collections = [c.name for c in collections]
+
+    if collection_name in existing_collections:
+        client.delete_collection(collection_name=collection_name)
+
     vector_store = QdrantVectorStore.from_documents(
         documents=texts,
         embedding=embeddings,
         url="http://localhost:6333",
-        collection_name=f"resume_{user_id}",
+        collection_name=collection_name,
     )
     return {"message": "Resume stored successfully", "collection": f"resume_{user_id}"}
