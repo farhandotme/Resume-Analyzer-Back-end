473394c7b4a7e7023e32d5b6593411f9a35bb85d
farhandotme <princeofficial355@gmail.com>
2026-05-21 21:55:18 +0530

    created the profile router and controller

--- app_backend/src/routes/userRoutes.ts
+++ app_backend/src/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import { login, signUp } from "../controllers/userControllers";
+import { login, profile, signUp } from "../controllers/userControllers";
+import { verifyToken } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/signup", signUp);
 router.post("/login", login);
+router.post("/profile", verifyToken, profile);
 
 export default router;
